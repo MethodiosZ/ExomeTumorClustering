a04078b619d17b44f546c028cacc4c7f58947f62
Giteutakis <72558695+MethodiosZ@users.noreply.github.com>
2025-03-04 19:11:26 +0200

    Update Normalize.sh

--- Normalize.sh
+++ Normalize.sh
@@ -3,5 +3,7 @@ for line in $(cat "snps.txt"); do
   name="${name//./}"
   echo "Normalize running on $name"
   bcftools norm --write-index -m - -f "../ref.fna.bgz" "$line" -O b -o "${name}_norm.bcf"
+  bcftools view --write-index -v snps "${name}_norm.bcf" -O b -o "{name}_snps.bcf"
+  rm "$name"_norm.bcf "$name"_norm.bcf.csi
   echo "Normalize on $name finished."
 done
